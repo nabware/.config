71936fc1b064237177f3aa232f6ee951bc3192b4
Nabeel Rana <nabwaregroup@gmail.com>
2024-04-07 13:42:35 -0700

    Remove unnecessary quotes

--- waybar/hwmon_dir.sh
+++ waybar/hwmon_dir.sh
@@ -3,8 +3,8 @@
 target_device_name=$1
 
 for curr_device_dir in /sys/class/hwmon/hwmon*; do
-    curr_device_name=$(cat "$curr_device_dir/name")
-    if [[ "$curr_device_name" == $target_device_name ]]; then
+    curr_device_name=$(cat $curr_device_dir/name)
+    if [[ $curr_device_name == $target_device_name ]]; then
         echo $curr_device_dir
         exit
     fi
